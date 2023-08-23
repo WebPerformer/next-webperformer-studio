04d51254d0d10abacd7a119806d042dbb1a3ca24
WebPerformer <webperformerz@gmail.com>
2023-08-23 14:20:46 -0300

    Some updates on Branding section

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -6,11 +6,12 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 
 // Images
 import star from '../../public/images/star.svg'
+import lines from '../../public/images/lines-2.svg'
 
 // Fonts
 import localFont from 'next/font/local'
-const panchang = localFont({
-  src: '../../public/fonts/Panchang-Variable.ttf',
+const walsheim = localFont({
+  src: '../../public/fonts/GT-Walsheim-Condensed-Medium.woff2',
 })
 
 export default function Branding() {
@@ -21,30 +22,31 @@ export default function Branding() {
     offset: ['0 1', '0 0.2'],
   })
 
-  const widthProgress = useTransform(scrollYProgress, [0, 1], ['80%', '100%'])
+  const widthProgress = useTransform(scrollYProgress, [0, 1], ['70%', '100%'])
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex justify-center overflow-hidden">
       <motion.div
         ref={ref}
         style={{
           width: widthProgress,
         }}
-        className="text-dark-500 bg-white rounded-2xl p-10"
+        className="relative text-dark-500 bg-white rounded-2xl p-10 overflow-hidden z-10"
       >
         <div>
           <Image src={star} alt="" className="w-6" />
           <h1
-            className={clsx(panchang.className, 'text-xl font-semibold my-10')}
+            className={clsx(walsheim.className, 'text-xxl font-semibold my-10')}
           >
             Unleash Your Imagination <br /> with Unimaginable Projects
           </h1>
           <p className="w-1/2 text-base text-gray translate-x-1/2">
             Step into a realm of boundless creativity and groundbreaking ideas.
-            Our passion for pushing the boundaries knows no limits, and our
-            portfolio of unimaginable projects is a testament to our unwavering
-            commitment to innovation.
+            Our passion for pushing the boundaries knows no limits.
           </p>
+        </div>
+        <div className="absolute -top-1/2 -right-[80px] h-screen overflow-hidden -z-10">
+          <Image src={lines} alt="" className="scale-105" />
         </div>
       </motion.div>
     </div>
